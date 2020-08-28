9f24bcc0d2f9ee102d21596be4e981266a831504
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-08-27 18:11:46 -0700

    Q2QueueUsingStacks

---
+++ Q2ImplementQueueUsingStacks/Q2ImplementQueueUsingStacks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace Q2ImplementQueueUsingStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new Queue();
+            q.Enqueue('a');
+            q.Enqueue('b');
+            q.Enqueue('c');
+            Console.WriteLine($"{q.Dequeue()} ");
+            q.Enqueue('d');
+            Console.WriteLine(q.Count());
+            Console.WriteLine($"{q.Dequeue()} ");
+            Console.WriteLine(q.Count());
+
+            Console.Write($"{q.Dequeue()} ");
+            Console.Write($"{q.Dequeue()} ");
+            Console.Write($"{q.Dequeue()} ");
+        }
+
+        public class Queue
+        {
+            public Stack s1 = new Stack();
+            public Stack s2 = new Stack();
+
+            public void Enqueue(char e)
+            {
+                s1.Push(e);
+            }
+
+            public char Dequeue()
+            {
+                if (s2.Count == 0)
+                {
+                    while(s1.Count != 0)
+                    {
+                        s2.Push(s1.Pop());
+                    }
+                }
+                return (s2.Count == 0) ? '\0' :(char)s2.Pop();
+            }
+
+            public int Count()
+            {
+                return s1.Count + s2.Count;
+            }
+        }
+    }
+}
